bccfd5c037ceaf82f1e7d873e3daf3feaa6626d2
Shpend Mula <shmula@hotmail.com>
2021-04-15 13:42:26 +0200

    updated script

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@
     //Prism.highlightAll(); // This works!
 
     // Kata class: creates a new Kata
-
+    
     class Kata {
-        constructor(title, url, yourSolution, altSolution) {
+        constructor(title, url, yourSolution, altSolution,timestamp) {
             this.title = title;
             this.url = url;
             this.yourSolution = yourSolution;
             this.altSolution = altSolution;
+            this.timestamp = timestamp;
         }
     }
 
@@ -26,6 +27,7 @@
         static addToCollection(kata) {
           const div = document.createElement('div');
           div.className = "kata";
+          div.dataset.timestamp = kata.timestamp;
           const h3 = document.createElement('h3');
           h3.textContent = kata.title;
           const i = document.createElement('i');
@@ -36,8 +38,8 @@
 
         }
 
-        static deleteKata() {
-
+        static deleteKata(ev) {
+            ev.target.parentNode.remove();
         }
 
         static showAlert(message, className) {
@@ -49,20 +51,27 @@
 
             setTimeout(()=> {
                 document.querySelector('.alert').remove();
-            },3000)
+            },2000)
         }
 
         static showKataDetails() {
-
+            document.querySelector('.store-kata-page').style.display = 'none';
+            document.querySelector('.display-kata-page').style.display = 'block';
         }
 
         static clearFields() {
+            document.querySelector('#kata-form').reset();
+        }
 
+        static addActiveClass(el) {
+            let kataCollection = document.querySelectorAll('.kata');
+            Array.from(kataCollection).forEach(kata => kata.classList.remove('selected-kata'));
+            el.classList.add('selected-kata');
         }
 
     }
 
-   //Store class: Handels all tasks with local storage
+   //Store class: Handles all tasks with local storage
 
     class Store {
         static getKata() {
@@ -80,6 +89,14 @@
             katas.push(kata)
             localStorage.setItem('kata', JSON.stringify(katas))
         }
+
+        static removeKata(kata) {
+           let id = kata.getAttribute("data-timestamp");
+           let katas = Store.getKata();
+           
+           katas = katas.filter(kata => kata.timestamp != id);
+           localStorage.setItem('kata', JSON.stringify(katas));
+        }
     }
 
     
@@ -91,15 +108,43 @@
         const url = document.querySelector('#kata-url').value;
         const yourSolution = document.querySelector('#kata-solution').value;
         const altSolution = document.querySelector('#alt-kata-solution').value;
+        const timestamp = Date.now();
+       
+        
 
         if(title === "" || url === "" || yourSolution === "" || altSolution === "") {
             UI.showAlert("Please fill in all the fields","danger");
         } else {
-            const kata = new Kata(title, url, yourSolution,altSolution);
+            const kata = new Kata(title, url, yourSolution,altSolution,timestamp);
             UI.addToCollection(kata);
             Store.storeKata(kata);
+            UI.showAlert('Kata added to collection', 'success');  
+            UI.clearFields();
         }
-
     })
 
-    document.addEventListener('DOMContentLoaded',UI.displayKatas)
+    // Event: Pull items from storage and add to list when page loads
+
+    document.addEventListener('DOMContentLoaded',UI.displayKatas);
+
+    // Event: Delete book
+    
+    document.querySelector('.katas-side-menu').addEventListener('click',(ev) => {
+        if(ev.target.classList.contains('delete')) {
+            UI.deleteKata(ev);
+            Store.removeKata(ev.target.parentNode);
+            UI.showAlert("Kata removed from collection", "success");
+        } else {
+            return;
+        }
+        
+    });
+
+    // Event: Show Kata Solutions 
+
+    document.querySelector('.katas-side-menu').addEventListener('click',(ev) => {
+            UI.showKataDetails(ev.target.closest('div.kata'));
+            UI.addActiveClass(ev.target.closest('div.kata'));
+    })
+
+
